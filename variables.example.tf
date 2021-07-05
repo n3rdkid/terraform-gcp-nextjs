eedfd2d5d6492d3e8cb9086dd9639016d3593ed3
Saurav Adhikari <sauravads123@gmail.com>
2021-07-05 18:46:41 +0545

    chore : added variables exapmple

---
+++ variables.example.tf
@@ -0,0 +1,23 @@
+variable "credentials"{
+    default = "XXXX.json"
+}
+
+variable "project"{
+    default = "XXXXX"
+}
+
+variable "region"{
+    default = "XXXXXX"
+}
+
+variable "zone"{
+    default = "XXXXXX"
+}
+
+variable "artifacts_location"{
+    default = "gs://bucket-name/path/to/somewhere"
+}
+
+variable "artifacts_url"{
+    default = "https://storage.googleapis.com/bucket-name/path/to/somewhere/artifact.zip"
+}
