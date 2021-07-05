c36b58f7807fd24ac211a4c1b74a2ab34a50adff
Saurav Adhikari <sauravads123@gmail.com>
2021-07-05 17:21:53 +0545

    test

--- client/pages/index.js
+++ client/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi, You are looking at Magic!
         </h1>
 
         <p className={styles.description}>
